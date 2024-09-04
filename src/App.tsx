8e78ae73bef6b55fe4338c20278eb3589208d58f
Alex <43609447+PegasusXing@users.noreply.github.com>
2024-09-05 00:13:02 +0200

    Readme file updates Licensing information added New branch for app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'> Hello world! </h1>
+      <h1 className='text-3xl font-bold underline'> Hello world! Are you ready? </h1>
     </>
   )
 }
